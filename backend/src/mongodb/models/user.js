1181add9ab4ceb1e80f627b30e133c95129996f2
Doğuhan İlter <doguhannilt@gmail.com>
2024-05-09 00:57:12 +0300

    user schema added

---
+++ backend/src/mongodb/models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+})
+
+// pass -> John
+userSchema.pre("save", async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password,8)
+    }
+    next()
+})
+// hash pass-> J3284Omv@fkjdkgN
+
+
+const UserModel = mongoose.model('User', userSchema)
+
+module.exports = UserModel
