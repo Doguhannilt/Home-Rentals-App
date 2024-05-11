070c6ee0632e2d71035f2e9c8186c56e3d81fea9
Doğuhan İlter <doguhannilt@gmail.com>
2024-05-11 03:31:29 +0300

    Login page template added

--- backend/index.js
+++ backend/index.js
@@ -25,8 +25,10 @@ app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials:true,
+    origin: "http://localhost:5173",
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST'],
 }));
 
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+
   return (
-    <div>
+    <div  style={{ backgroundImage: `url("/images/main.jpg")`, backgroundSize: 'cover', backgroundPosition: 'center' }} className="h-screen">
+          <div className = "">
+        <form className="mx-auto max-w-md flex flex-col gap-10 pt-40 font-bold h-full w-full ">
+           
+        <h2 className="text-3xl font-bold text-white">Login 🌴</h2>
+        {/* Email */}
+        <label className="text-white text-md font-bold flex-1">
+          Email
+          <input
+            type="email"
+            className="text-white text-2xl rounded w-full h-full py-1 px-2 font-normal hover:border-none bg-gray-500/50"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+     
+        </label>
+        {/* Password */}
+        <label className="text-white text-md font-bold ">
+          Password
+          <input
+            type="password"
+              className="text-white text-2xl rounded w-full h-full py-1 px-2 font-normal hover:border-none bg-gray-500/50 "
+              onChange={(e) => setPassword(e.target.value)}
+            />
+        {password.length < 6 && <p>Password must be at least 6 characters</p>}
       
+            
+        </label>
+        <div className = "flex gap-24 ">
+        <span>
+        <button type="submit" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Register</button>
+        </span>
+        <span>
+            <label className = "text-gray-800"><a href="/register">Don't you have an account?</a></label>
+        </span>
+        </div>
+       
+      </form>
+              </div>
     </div>
   )
 }
